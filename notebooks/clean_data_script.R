9cd9d18e88fee7d7b458f7571580170e81efb52b
c-a-s-t-l-e <castle676767@gmail.com>
2025-09-09 00:53:27 -0400

    added lines of code  to be able to get the first version of the report and subset the columns

--- notebooks/clean_data_script.R
+++ notebooks/clean_data_script.R
@@ -7,3 +7,20 @@ ads_data <- read_csv("./data/raw/SGO-ADS-crash-data.csv")
 # clean up the column names
 ads_data <- clean_names(ads_data)
 
+# get a subset of the data
+subset_df <- ads_data |> 
+  filter(
+    report_version == 1   # get just the first version of earch report
+  ) |> 
+  select(                 # select the columns we want
+    reporting_entity,
+    make,
+    model,
+    model_year,
+    incident_date,
+    incident_time_24_00,
+    roadway_type,
+    sv_pre_crash_movement,
+    sv_precrash_speed_mph
+  )
+
